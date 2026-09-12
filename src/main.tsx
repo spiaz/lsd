811c51c2fc922bf12c025640ade4a15b4cf70389
Stefano Piazza <stefano.piazza@nexthink.com>
2026-09-13 01:42:50 +0200

    Restore Dracula palette

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
 import './styles.css';
-import './alucard.css';
 import './mvp-navigation.css';
 
 registerSW({ immediate: true });
